ccdb37411f2523fd00d8eb66454a4abc788430cc
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-12-04 14:27:55 -0300

    começando o requisito 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Wallet from './pages/Wallet';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <main>
+      <h1>Hello, TrybeWallet!</h1>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/carteira" element={ <Wallet /> } />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,46 @@
+import { Link } from 'react-router-dom';
+
 function Login() {
   return (
     <>
-      <h1> Login </h1>
-      <button>
-        Entrar
-      </button>
+      <h3> Login </h3>
+      <label
+        htmlFor="e-mail"
+      >
+        Insira seu e-mail
+        <input
+          placeholder="usuario@provedor.com"
+          type="e-mail"
+          data-testid="email-input"
+          required
+          name="email"
+          // value={ email }
+          // onChange={ handleChange }
+        />
+      </label>
+      <label
+        htmlFor="password"
+      >
+        Insira sua senha
+        <input
+          placeholder="123456"
+          type="password"
+          data-testid="password-input"
+          required
+          name="password"
+          // value={ password }
+          // onChange={ handleChange }
+        />
+      </label>
+      <Link
+        to="/carteira"
+      >
+        <button
+          disabled
+        >
+          Entrar
+        </button>
+      </Link>
     </>
   );
 }
