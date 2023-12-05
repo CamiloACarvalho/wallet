af7b68246f015b540d8d9704868064c0b9150b13
CamiloACarvalho <camilo.carvalho@engenharia.ufjf.br>
2023-12-04 23:47:27 -0300

    requisito 01 5/6

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,61 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
+  // Criando o link para navegar para outra página quando fizer o login
+  const navigate = useNavigate();
+
+  // Validando o login e senha
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Capturando a informação digitada para efetuar a validação
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  // Capturando a informação digitada para efetuar a validação
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  // Criando uma função para validar o login e senha do usuário
+  const loginValidation = () => {
+    // Criando uma validação para email no regex no formato usuario@provedor.com
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    // Fazendo o teste, se passar o valor retornar é true
+    const validEmail = emailRegex.test(email);
+
+    // vou fazer a mesma coisa para a senha. O único requisito é que seja maior que 6 caracteres
+    const passwordRegex = /^.{6,}$/;
+    const validPassword = passwordRegex.test(password);
+
+    // Retorna true, e ai habilita do botão de login
+    return (validEmail && validPassword);
+  };
+
+  // Criando uma função para submit
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate('/carteira');
+  };
+
   return (
-    <>
-      <h3> Login </h3>
-      <label
-        htmlFor="e-mail"
-      >
+    <form onSubmit={ handleSubmit }>
+      <h3>Login</h3>
+      <label htmlFor="email">
         Insira seu e-mail
         <input
           placeholder="usuario@provedor.com"
-          type="e-mail"
+          type="email"
           data-testid="email-input"
           required
           name="email"
-          // value={ email }
-          // onChange={ handleChange }
+          value={ email }
+          onChange={ handleEmailChange }
         />
       </label>
-      <label
-        htmlFor="password"
-      >
+      <label htmlFor="password">
         Insira sua senha
         <input
           placeholder="123456"
@@ -28,20 +63,17 @@ function Login() {
           data-testid="password-input"
           required
           name="password"
-          // value={ password }
-          // onChange={ handleChange }
+          value={ password }
+          onChange={ handlePasswordChange }
         />
       </label>
-      <Link
-        to="/carteira"
+      <button
+        type="submit"
+        disabled={ !loginValidation() }
       >
-        <button
-          disabled
-        >
-          Entrar
-        </button>
-      </Link>
-    </>
+        Entrar
+      </button>
+    </form>
   );
 }
 
